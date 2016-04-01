29cf27748ed60594d4eb687b05a6e0ca9ea05225
Oleg SKLYANCHUK <sklyanchuk@gmail.com>
2016-04-01 12:01:43 +0200

    Fix ratio count in stats component.
    Count 0/0 as 100% coverage instead of 0%.

--- src/webapp/components/stats/stats.js
+++ src/webapp/components/stats/stats.js
@@ -5,7 +5,7 @@ import getMetricTagTitle from '../../services/get-metric-tag-title';
 function generateStats(metrics) {
   return Object.keys(metrics).map(tag => {
     const {passed, total} = metrics[tag];
-    const ratio = total ? passed / total : 0;
+    const ratio = total ? passed / total : 1;
     return h('div', {className: 'stat'}, [
       h('div', {className: 'tagName'}, [getMetricTagTitle(tag)]),
       h('div', {className: 'tagPercentage'}, [toPercentage(ratio)]),
